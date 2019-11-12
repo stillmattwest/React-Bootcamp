28090e696ec2a166987202e7bed0faf8c246b45d
stillmattwest <stillmattwest@gmail.com>
2019-11-12 14:44:15 -0800

    adding stateClicker project

---
+++ StateClicker/state-clicker/src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+import StateClicker from '../StateClicker/stateClicker';
+
+function App() {
+  return (
+    <div className="App">
+      <StateClicker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ StateClicker/state-clicker/src/Components/StateClicker/stateClicker.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+
+class StateClicker extends Component{
+    state = {
+        num:null,
+        win:false
+    }
+
+    handleClick = e => {
+        let luckyNumber = 7;
+        let rand = this.getRandom();
+        if(rand === luckyNumber){
+            this.setState({num:rand,win:true});
+        }
+        
+        this.setState({num:rand});
+        return;
+    }
+
+    getRandom = () => {
+        let max = 10;
+        let rand = (1+(Math.floor(Math.random() * max)));
+        return rand;
+    }
+
+    render(){
+        return(
+            <div>
+            <h1>Your number is: {this.state.num}</h1>
+            {this.state.win ? <h2>You win!!!</h2> : <button onClick = {this.handleClick}>Click Me!</button>}
+            </div>
+        )
+    }
+}
+
+export default StateClicker;
