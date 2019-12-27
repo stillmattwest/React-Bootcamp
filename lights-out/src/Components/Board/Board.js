611105230ae2d325e044b4033f49f96efbd452a3
stillmattwest <stillmattwest@gmail.com>
2019-12-26 16:26:36 -0800

    adding gameState to lights out project

--- lights-out/src/Components/Board/Board.js
+++ lights-out/src/Components/Board/Board.js
@@ -3,6 +3,12 @@ import './Board.css';
 import Cell from '../Cell/Cell';
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameState: this.generateGameStateGrid()
+    };
+  }
   generateBoard = () => {
     // generates a 5x5 table
     return Array.from(Array(this.props.numRows)).map((row, rowNum) => {
@@ -19,9 +25,18 @@ class Board extends Component {
       );
     });
   };
+
+  generateGameStateGrid = () => {
+    return Array.from(Array(this.props.numRows)).map(row => {
+      return Array.from(Array(this.props.numColumns)).map((e, i) => {
+        return 0;
+      });
+    });
+  };
   render() {
     return (
       <div className='board-container'>
+        {console.log('gameState: ', this.state.gameState)}
         <table>
           <tbody>{this.generateBoard()}</tbody>
         </table>
