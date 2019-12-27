33ef13d46854cbf5c1f04b0014b3e7de3bca4260
stillmattwest <stillmattwest@gmail.com>
2019-12-27 14:18:09 -0800

    added a whole bunch of code

--- lights-out/src/Components/Board/Board.js
+++ lights-out/src/Components/Board/Board.js
@@ -6,18 +6,31 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gameState: this.generateGameStateGrid()
+      gameState: this.generateGameStateGrid(),
+      started: false,
+      won: false
     };
   }
+  componentDidMount() {
+    if (!this.state.started) {
+      this.lightInitialCells();
+    }
+    this.setState({ started: true });
+  }
   generateBoard = () => {
-    // generates a 5x5 table
+    // generates a numRows x numColumns table
     return Array.from(Array(this.props.numRows)).map((row, rowNum) => {
       return (
         <tr key={`row-${rowNum}`}>
           {Array.from(Array(this.props.numColumns)).map((col, colNum) => {
             return (
               <td key={`${rowNum}-${colNum}`}>
-                <Cell />
+                <Cell
+                  row={rowNum}
+                  col={colNum}
+                  lit={this.state.gameState[rowNum][colNum]}
+                  toggleCell={this.toggleCell}
+                />
               </td>
             );
           })}
@@ -33,11 +46,60 @@ class Board extends Component {
       });
     });
   };
+
+  getRandom = max => {
+    return Math.floor(1 + Math.random() * max) - 1;
+  };
+
+  lightInitialCells = () => {
+    let initialCells = [];
+    let numLights = Math.ceil(this.props.numRows / 2);
+    for (let i = 0; i < numLights; i++) {
+      initialCells.push({
+        row: this.getRandom(this.props.numRows),
+        col: this.getRandom(this.props.numColumns)
+      });
+    }
+    let newGameState = JSON.parse(JSON.stringify(this.state.gameState));
+    initialCells.forEach(cellObj => {
+      let row = cellObj.row;
+      let col = cellObj.col;
+      newGameState[row][col] = newGameState[row][col] ? 0 : 1;
+    });
+    this.setState({ gameState: JSON.parse(JSON.stringify(newGameState)) });
+  };
+
+  toggleCell = (row, col) => {
+    let newGameState = JSON.parse(JSON.stringify(this.state.gameState));
+    // toggle cell
+    newGameState[row][col] = newGameState[row][col] ? 0 : 1;
+    // toggle cell above
+    if (row > 0) {
+      newGameState[row - 1][col] = newGameState[row - 1][col] ? 0 : 1;
+    }
+    // toggle cell below
+    if (row < this.props.numRows - 1) {
+      newGameState[row + 1][col] = newGameState[row + 1][col] ? 0 : 1;
+    }
+    // toggle cell to left
+    if (col > 0) {
+      newGameState[row][col - 1] = newGameState[row][col - 1] ? 0 : 1;
+    }
+    // toggle cell to right
+    if (col < this.props.numColumns - 1) {
+      newGameState[row][col + 1] = newGameState[row][col + 1] ? 0 : 1;
+    }
+    this.setState({ gameState: JSON.parse(JSON.stringify(newGameState)) });
+  };
+
   render() {
     return (
       <div className='board-container'>
-        {console.log('gameState: ', this.state.gameState)}
-        <table>
+        <h2 className='board-title'>
+          <span className='board-neon-red'>Lights</span>
+          <span className='board-neon-yellow'>Out</span>
+        </h2>
+        <table className='board-table'>
           <tbody>{this.generateBoard()}</tbody>
         </table>
       </div>
--- lights-out/src/Components/Cell/Cell.js
+++ lights-out/src/Components/Cell/Cell.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import './Cell.css';
 
 class Cell extends Component {
+  handleClick = (row, col) => {
+    this.props.toggleCell(row, col);
+  };
   render() {
-    return <div className='cell'></div>;
+    return (
+      <div
+        className={`cell ${this.props.lit ? 'cell-lit' : ''}`}
+        onClick={() => this.handleClick(this.props.row, this.props.col)}
+      ></div>
+    );
   }
 }
 
